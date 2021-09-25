03b02dc68078bc85fa738e501a7b0bee527d1085
risako.yamamoto <en42178@gmail.com>
2021-09-25 02:34:28 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,8 +10,12 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- メッセージ作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '投稿', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                </ul>
+           
             </ul>
         </div>
     </nav>
